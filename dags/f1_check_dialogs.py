e26d3c480089901c94349fbd75229e7e151dcb39
Denis Mironov <mironovdenis@list.ru>
2024-06-24 20:41:42 +0300

    working version 0.11

--- dags/f1_check_dialogs.py
+++ dags/f1_check_dialogs.py
@@ -1,111 +1,150 @@
 from airflow import DAG
-from airflow.decorators import task
+from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 import os
 import logging
 
-from tg_jobs_parser.telegram_helper.channel_parser import ChannelParser
+from tg_jobs_parser.telegram_helper.telegram_parser import TelegramParser
 from tg_jobs_parser.google_cloud_helper.storage_manager import StorageManager
 from tg_jobs_parser.utils import json_helper
 from tg_jobs_parser.configs import vars, volume_folder_path
-from tg_jobs_parser.telegram_helper.message_parser import MessageParser
+
+# Initialize necessary components
+logging.info('Initializing TelegramParser and StorageManager')
+storage_manager = StorageManager()
+tg_parser = TelegramParser()
+
+# Define paths
+CL_CHANNELS_LOCAL_PATH = os.path.join(volume_folder_path, 'f1.1_gsc_channels_metadata.json')
+TG_CHANNELS_LOCAL_PATH = os.path.join(volume_folder_path, 'f1.1_tg_channels_metadata.json')
+MG_CHANNELS_LOCAL_PATH = os.path.join(volume_folder_path, 'f1.1_merged_channels_metadata.json')
 
 
-# Путь к локальным файлам метаданных
-CL_CHANNELS_LOCAL_PATH = os.path.join(volume_folder_path, 'gsc_channels_metadata.json')
-TG_CHANNELS_LOCAL_PATH = os.path.join(volume_folder_path, 'tg_channels_metadata.json')
-MG_CHANNELS_LOCAL_PATH = os.path.join(volume_folder_path, 'merged_channels_metadata.json')
-
-
-class Context:
-    def __init__(self):
-        self.storage_manager = StorageManager()
-        self.channel_parser = ChannelParser()
-        self.msg_parser = MessageParser()
-
-
-ctx = Context()
-
-# Определяем задачи DAG
-@task
-def init_channel_parser_and_parse_tg_dialog():
-    logging.info('Initializing channel parser and parsing Telegram dialog')
-    dialogs = ctx.channel_parser.get_channels()
-    json_helper.save_to_json(data=dialogs, path=TG_CHANNELS_LOCAL_PATH)
-    logging.info(f'Saved dialogs to {TG_CHANNELS_LOCAL_PATH}')
-    return TG_CHANNELS_LOCAL_PATH
-
-@task
-def get_metadata_from_cloud():
-    logging.info('Getting metadata from cloud')
-    ctx.storage_manager.download_channels_metadata(path=CL_CHANNELS_LOCAL_PATH)
-    logging.info(f'Downloaded metadata to {CL_CHANNELS_LOCAL_PATH}')
-    return CL_CHANNELS_LOCAL_PATH
-
-@task
-def update_metadata_locally(date=vars.START_DATE, force=False):
-    logging.info(f'Updating metadata locally. Attrs ( date: {date}, force: {force})')
-    cloud_channels = json_helper.read_json(CL_CHANNELS_LOCAL_PATH) or {}
-    tg_channels = json_helper.read_json(TG_CHANNELS_LOCAL_PATH)
+def parse_tg_dialogs():
     try:
-        json_helper.refresh_status(tg_channels, cloud_channels, ctx.msg_parser, date, force)
-    finally:
+        logging.info('Start parsing tg dialogs')
+        json_helper.save_to_json(data=tg_parser.get_channels(), path=TG_CHANNELS_LOCAL_PATH)
+        logging.info(f'Saved dialogs to {TG_CHANNELS_LOCAL_PATH}')
+        return True
+    except Exception as e:
+        raise Exception(f'Error Getting metadata from cloud: {e}')
+
+
+def get_metadata_from_cloud():
+    try:
+        logging.info('Getting metadata from cloud')
+        storage_manager.download_channels_metadata(path=CL_CHANNELS_LOCAL_PATH)
+        return True
+    except Exception as e:
+        raise Exception(f'Error Getting metadata from cloud: {e}')
+
+
+def update_target_ids(date=vars.START_DATE, force=False):
+    try:
+        logging.info(f'Updating metadata locally. Attrs ( date: {date}, force: {force})')
+        cloud_channels = json_helper.read_json(CL_CHANNELS_LOCAL_PATH) or {}
+        tg_channels = json_helper.read_json(TG_CHANNELS_LOCAL_PATH)
+        # set to cloud_channels target id from parsed dialogs
+        json_helper.set_target_ids(tg_channels, cloud_channels, tg_parser, date, force)
         json_helper.save_to_json(cloud_channels, CL_CHANNELS_LOCAL_PATH)
         logging.info(f'Saved updated metadata to {CL_CHANNELS_LOCAL_PATH}')
-        return CL_CHANNELS_LOCAL_PATH
+        return True
+    except Exception as e:
+        raise Exception(f'Error Updating metadata locally: {e}')
 
 
-@task
-def merge_metadata():
-    logging.info('Merging metadata')
-    json_helper.merge_json_files(file1_path=CL_CHANNELS_LOCAL_PATH, file2_path=TG_CHANNELS_LOCAL_PATH,
-                                 output_path=MG_CHANNELS_LOCAL_PATH)
-    logging.info(f'Merged metadata saved to {MG_CHANNELS_LOCAL_PATH}')
-    return MG_CHANNELS_LOCAL_PATH
+def update_last_updated_ids():
+    try:
+        logging.info('Merging metadata to update last posted ids in tg cgannels')
+        json_helper.merge_json_files(file1_path=CL_CHANNELS_LOCAL_PATH, file2_path=TG_CHANNELS_LOCAL_PATH,
+                                     output_path=MG_CHANNELS_LOCAL_PATH)
+        logging.info(f'Merged metadata saved to {MG_CHANNELS_LOCAL_PATH}')
+        return True
+    except Exception as e:
+        raise Exception(f'Error merging metadata locally: {e}')
 
-@task
+
 def update_metadata_in_cloud():
-    logging.info('Updating metadata in cloud')
-    ctx.storage_manager.update_channels_metadata(source_file_name=MG_CHANNELS_LOCAL_PATH)
-    logging.info(f'Updated cloud metadata with {MG_CHANNELS_LOCAL_PATH}')
+    try:
+        logging.info('Updating metadata in cloud')
+        storage_manager.update_channels_metadata(source_file_name=MG_CHANNELS_LOCAL_PATH)
+        logging.info(f'Updated cloud metadata with {MG_CHANNELS_LOCAL_PATH}. Now you can use it')
+        return True
+    except Exception as e:
+        raise Exception(f'Error Updating metadata in cloud: {e}')
 
-@task
+
+def delete_tmp_files():
+    try:
+        os.remove(CL_CHANNELS_LOCAL_PATH)
+        os.remove(TG_CHANNELS_LOCAL_PATH)
+        os.remove(MG_CHANNELS_LOCAL_PATH)
+        print("Files deleted successfully.")
+    except FileNotFoundError:
+        print("One or more files were not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
 def check_unparsed_msgs():
-    """
-    just check how much messages we have
-    :return:
-    """
-    ctx.storage_manager.download_channels_metadata(path=CL_CHANNELS_LOCAL_PATH)
-    channels = json_helper.read_json(CL_CHANNELS_LOCAL_PATH)
-    json_helper.calc_stats(channels)
+    try:
+        logging.info('Checking stats')
+        storage_manager.check_channel_stats()
+        return True
+    except Exception as e:
+        raise Exception(f'Error Checking stats: {e}')
 
 
-# Определяем параметры по умолчанию для DAG
 default_args = {
     'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
     'start_date': datetime(2024, 6, 1),
 }
-# Определяем DAG
-with DAG(
-    'f1_check_dialogs',
-    default_args=default_args,
-    schedule_interval='0 */3 * * *',
-    catchup=False,
-    max_active_runs=1,
-    tags=['f1', 'message-processing', 'gcs', 'tg_jobs_parser']
-) as dag:
-    # Определяем порядок выполнения задач
-    tg_dialog_task = init_channel_parser_and_parse_tg_dialog()
-    cloud_metadata_task = get_metadata_from_cloud()
-    update_local_metadata_task = update_metadata_locally()
-    merge_metadata_task = merge_metadata()
-    update_cloud_metadata_task = update_metadata_in_cloud()
-    stats_task = check_unparsed_msgs()
 
-    # Задаем зависимости между задачами
-    tg_dialog_task >> cloud_metadata_task >> update_local_metadata_task >> merge_metadata_task >> update_cloud_metadata_task >> stats_task
+with DAG(
+        'f1_check_dialogs',
+        default_args=default_args,
+        schedule='0 */3 * * *',
+        catchup=False,
+        max_active_runs=1,
+        tags=['f1', 'message-processing', 'gcs', 'tg_jobs_parser']
+) as dag:
+    parse_tg_dialogs_task = PythonOperator(
+        task_id='t1_parse_tg_dialogs',
+        python_callable=parse_tg_dialogs
+    )
+
+    get_cloud_metadata_task = PythonOperator(
+        task_id='t2_get_metadata_from_cloud',
+        python_callable=get_metadata_from_cloud
+    )
+
+    update_target_ids_task = PythonOperator(
+        task_id='t3_update_target_ids',
+        python_callable=update_target_ids
+    )
+
+    update_last_updated_ids_task = PythonOperator(
+        task_id='t4_update_last_updated_ids',
+        python_callable=update_last_updated_ids
+    )
+
+    update_cloud_metadata_task = PythonOperator(
+        task_id='t5_update_metadata_in_cloud',
+        python_callable=update_metadata_in_cloud
+    )
+
+    delete_tmp_files_task = PythonOperator(
+        task_id='t6_delete_tmp_files',
+        python_callable=delete_tmp_files,
+    )
+
+    stats_task = PythonOperator(
+        task_id='t7_check_unparsed_msgs',
+        python_callable=check_unparsed_msgs
+    )
+
+    parse_tg_dialogs_task >> get_cloud_metadata_task >> update_target_ids_task \
+        >> update_last_updated_ids_task >> update_cloud_metadata_task >> delete_tmp_files_task >> stats_task
+
+if __name__ == '__main__':
+    dag.test()
